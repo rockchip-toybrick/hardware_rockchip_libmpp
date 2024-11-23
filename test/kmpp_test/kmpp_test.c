0ae8c4536d151df1543802236422cb08a2951740
Herman Chen <herman.chen@rock-chips.com>
2024-11-23 16:07:59 +0800

    feat[test]: Add test module
    Change-Id: I6ad6b65710bcfbc0556c7926027b8f91c87410b0
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

---
+++ test/kmpp_test/kmpp_test.c
@@ -0,0 +1,199 @@
+/* SPDX-License-Identifier: ((GPL-2.0+ WITH Linux-syscall-note) OR MIT) */
+/*
+ * Copyright (c) 2024 Rockchip Electronics Co., Ltd.
+ */
+
+#define MODULE_TAG "kmpp_test"
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include "kmpp_osal.h"
+
+#include "kmpp_obj.h"
+#include "kmpp_shm.h"
+#include "kmpp_frame.h"
+
+static osal_worker *test_worker = NULL;
+static osal_work *test_work = NULL;
+
+typedef struct KmppCallback_t {
+    void *arg0;
+    rk_u32 arg1;
+    rk_u64 arg2;
+    rk_s32 (*func)(void *param);
+} KmppCallback;
+
+#define KMPP_OBJ_IMPL_TYPE  KmppCallback
+#define KMPP_OBJ_IMPL_DEF   kmpp_obj
+
+#include "kmpp_obj_helper.h"
+
+#define ENTRY_TO_TRIE(cls, field, type) \
+    do { \
+        KmppLocTbl tbl = FIELD_TO_LOCTBL_ACCESS1(field, type); \
+        kmpp_objdef_add_entry(cls, #field, &tbl); \
+    } while (0);
+
+
+#define ENTRY_TABLE(ENTRY, cls) \
+    ENTRY(cls, arg0, ptr) \
+    ENTRY(cls, arg1, u32) \
+    ENTRY(cls, arg2, u64) \
+    ENTRY(cls, func, ptr)
+
+static rk_s32 kmpp_show(void *param)
+{
+    KmppCallback *cb = (KmppCallback *)param;
+
+    kmpp_log_f("arg0 %px\n", cb->arg0);
+    kmpp_log_f("arg1 %d\n", cb->arg1);
+    kmpp_log_f("arg2 %lld\n", cb->arg2);
+
+    return 0;
+}
+
+ void kmpp_func_preset(void *obj)
+ {
+    KmppCallback *cb = (KmppCallback *)obj;
+
+    cb->arg0 = NULL;
+    cb->arg1 = 1234;
+    cb->arg2 = 5678;
+    cb->func = kmpp_show;
+ }
+
+static KmppObjDef kmpp_def = NULL;
+static KmppObjDef test_def = NULL;
+
+rk_s32 kmpp_test_func(void *param)
+{
+    KmppObj obj = NULL;
+    rk_s32 ret = rk_nok;
+    MppFrame frame;
+    KmppTrie trie1;
+    KmppTrie trie2;
+    void *root;
+
+    ret = kmpp_frame_get(&frame);
+    kmpp_logi_f("get frame %px ret %d\n", frame, ret);
+    if (ret)
+        goto done;
+
+    kmpp_frame_dump_f(frame);
+
+    ret = kmpp_frame_put(frame);
+    kmpp_logi_f("put frame %px ret %d\n", frame, ret);
+    if (ret)
+        goto done;
+
+    kmpp_objdef_init(&kmpp_def, sizeof(KmppCallback), "kmpp_call");
+    ENTRY_TABLE(ENTRY_TO_TRIE, kmpp_def);
+    kmpp_objdef_add_entry(kmpp_def, NULL, NULL);
+    kmpp_objdef_add_preset(kmpp_def, kmpp_func_preset);
+
+    kmpp_logi_f("obj init done %px\n", kmpp_def);
+
+    trie1 = kmpp_objdef_get_trie(kmpp_def);
+    if (!trie1) {
+        kmpp_loge("get trie failed\n");
+        goto done;
+    }
+
+    root = kmpp_trie_get_node_root(trie1);
+    if (!root) {
+        kmpp_loge("get trie root failed\n");
+        goto done;
+    }
+
+    ret = kmpp_trie_init(&trie2, "trie_import_test");
+    if (ret) {
+        kmpp_loge("trie2 init failed\n");
+        goto done;
+    }
+
+    ret = kmpp_trie_import(trie2, root);
+    if (ret) {
+        kmpp_loge("trie2 import failed\n");
+        goto done;
+    }
+
+    kmpp_trie_deinit(trie2);
+
+    kmpp_obj_get(&obj, kmpp_def);
+
+    kmpp_obj_set_ptr(obj, "arg0", NULL);
+    kmpp_obj_set_s32(obj, "arg1", 1);
+    kmpp_obj_set_u64(obj, "arg2", 2);
+    kmpp_obj_set_ptr(obj, "func", kmpp_show);
+
+    kmpp_logi_f("kmpp_show %px\n", kmpp_show);
+
+    kmpp_obj_run(obj, "func");
+
+    kmpp_obj_put(obj);
+
+    kmpp_objdef_lookup(&test_def, "kmpp_call");
+
+    kmpp_logi_f("lookup kmpp_call ret %px\n", test_def);
+
+    if (test_def) {
+        kmpp_logi_f("found kmpp_call\n");
+
+        kmpp_obj_get(&obj, test_def);
+        kmpp_logi_f("get kmpp_call ret %px\n", obj);
+        kmpp_obj_run(obj, "func");
+        kmpp_obj_put(obj);
+
+        kmpp_objdef_deinit(test_def);
+
+        ret = rk_ok;
+    }
+
+done:
+    kmpp_logi("kmpp test func done %s\n", ret ? "failed" : "success");
+
+    return ret;
+}
+
+void kmpp_test_prepare(void)
+{
+    kmpp_logi("kmpp test start\n");
+}
+
+void kmpp_test_finish(void)
+{
+    kmpp_logi("kmpp test end\n");
+}
+
+int kmpp_test_init(void)
+{
+    kmpp_test_prepare();
+
+    osal_worker_init(&test_worker, "kmpp test");
+    osal_work_init(&test_work, kmpp_test_func, NULL);
+
+    osal_work_queue(test_worker, test_work);
+
+    return 0;
+}
+
+void kmpp_test_exit(void)
+{
+    osal_worker_deinit(&test_worker);
+    osal_work_deinit(&test_work);
+
+    if (kmpp_def) {
+        kmpp_objdef_deinit(kmpp_def);
+        kmpp_def = NULL;
+    }
+
+    kmpp_test_finish();
+}
+
+module_init(kmpp_test_init);
+module_exit(kmpp_test_exit);
+
+MODULE_AUTHOR("rockchip");
+MODULE_LICENSE("proprietary");
+MODULE_VERSION("1.0");
---
+++ test/osal_test/osal_test.c
@@ -0,0 +1,224 @@
+/* SPDX-License-Identifier: ((GPL-2.0+ WITH Linux-syscall-note) OR MIT) */
+/*
+ * Copyright (c) 2024 Rockchip Electronics Co., Ltd.
+ */
+
+#define MODULE_TAG "osal_test"
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include "kmpp_osal.h"
+
+static osal_worker *test_worker = NULL;
+static osal_work *test_work = NULL;
+
+static osal_cls *test_cls = NULL;
+static osal_fs_dev_mgr *test_fs_dev_mgr = NULL;
+
+static rk_u32 test_env_u64 = 0;
+static rk_u32 test_env_u32 = 0;
+static char *test_env_str = "this is a test env string";
+
+rk_s32 node_thds_func(void *data)
+{
+    kmpp_thds *thd = (kmpp_thds *)data;
+
+    kmpp_logi("run idx %d\n", thd->idx);
+    return 0;
+}
+
+rk_s32 osal_test_open(osal_fs_dev *ctx)
+{
+    kmpp_logi_f("\n");
+    return rk_ok;
+}
+
+rk_s32 osal_test_release(osal_fs_dev *ctx)
+{
+    kmpp_logi_f("\n");
+    return rk_ok;
+}
+
+rk_s32 osal_test_ioctl(osal_fs_dev *ctx, rk_s32 cmd, void *arg)
+{
+    kmpp_logi_f("\n");
+    return rk_ok;
+}
+
+osal_dev_fops test_fops = {
+    .open       = osal_test_open,
+    .release    = osal_test_release,
+    .ioctl      = osal_test_ioctl,
+};
+
+#define DMABUF_COUNT 100
+#define DMABUF_SIZE  (720*480*2)
+
+void test_env_show(KmppEnvNode node, void *data)
+{
+    kmpp_env_log(node, "%s enter data %px\n", __FUNCTION__, data);
+}
+
+rk_s32 osal_test_func(void *param)
+{
+    osal_fs_dev_cfg cfg;
+    KmppEnvInfo info;
+    osal_file *tmp_file = NULL;
+    kmpp_thds *thds = NULL;
+    osal_dev *odev = NULL;
+    void *ptr = NULL;
+    void *ptr_shm = NULL;
+    rk_s32 i;
+
+    kmpp_logi("osal test func start\n");
+
+    ptr = osal_kmalloc(4096, osal_gfp_normal);
+    ptr_shm = osal_malloc_share(4096);
+
+    kmpp_logi("malloc get %px shm %px\n", ptr, ptr_shm);
+
+    kmpp_thds_init(&thds, "node_thds", 4);
+    kmpp_thds_setup(thds, node_thds_func, ptr);
+
+    for (i = 0; i < 20; i++)
+        kmpp_thds_run_nl(thds);
+
+    kmpp_logi("node init ptr %px\n", ptr);
+
+    kmpp_thds_deinit(&thds);
+
+    osal_kfree(ptr);
+    osal_kfree_share(ptr_shm);
+
+    tmp_file = osal_fopen("/sdcard/hello.txt", OSAL_O_CREAT | OSAL_O_RDWR, 0666);
+    if (tmp_file) {
+        char str[] = "hello world\n";
+        char buf[32];
+
+        osal_fwrite(tmp_file, str, osal_strlen(str));
+        osal_fseek(tmp_file, 0, OSAL_SEEK_SET);
+        osal_fread(tmp_file, buf, sizeof(buf));
+
+        printk("read file string: %s", buf);
+        osal_fclose(tmp_file);
+    } else {
+        printk("tmp_file is invalid\n");
+    }
+
+    kmpp_logi("osal class test start\n");
+
+    osal_class_init(&test_cls, "osal_test");
+
+    cfg.cls = test_cls;
+    cfg.name = "osal_fs_dev";
+    cfg.fops = &test_fops;
+    cfg.drv_data = NULL;
+    cfg.priv_size = 0;
+    cfg.buf = NULL;
+    cfg.size = 0;
+
+    osal_fs_dev_mgr_init(&test_fs_dev_mgr, &cfg);
+
+    kmpp_logi("fs dev %s registered manager %px\n", cfg.name, test_fs_dev_mgr);
+
+    odev = osal_dev_get("vdpp");
+    if (odev) {
+        osal_dmabuf *buf[DMABUF_COUNT];
+
+        kmpp_logi("osal dmabuf test start\n");
+
+        for (i = 0; i < DMABUF_COUNT; i++)
+            osal_dmabuf_alloc(&buf[i], odev, DMABUF_SIZE, 0);
+
+        for (i = 0; i < DMABUF_COUNT; i++) {
+            osal_memset(buf[i]->kaddr, 0xff, DMABUF_SIZE);
+
+            osal_dmabuf_sync_for_dev(buf[i], OSAL_DMA_TO_DEVICE);
+
+            osal_dmabuf_sync_for_cpu(buf[i], OSAL_DMA_FROM_DEVICE);
+        }
+
+        for (i = 0; i < DMABUF_COUNT; i++)
+            osal_dmabuf_free(buf[i]);
+
+        kmpp_logi("osal dmabuf test donet\n");
+    }
+
+    kmpp_logi("osal env test start\n");
+
+    info.name = "osal_test_u32";
+    info.readonly = 0;
+    info.type = KmppEnv_u32;
+    info.val = &test_env_u32;
+    info.env_show = NULL;
+
+    kmpp_env_add(kmpp_env_osal, NULL, &info);
+
+    info.name = "osal_test_u64";
+    info.readonly = 0;
+    info.type = KmppEnv_u64;
+    info.val = &test_env_u64;
+    info.env_show = NULL;
+
+    kmpp_env_add(kmpp_env_osal, NULL, &info);
+
+    info.name = "osal_test_str";
+    info.readonly = 0;
+    info.type = KmppEnv_str;
+    info.val = test_env_str;
+    info.env_show = NULL;
+
+    kmpp_env_add(kmpp_env_osal, NULL, &info);
+
+    info.name = "osal_test_user";
+    info.readonly = 0;
+    info.type = KmppEnv_user;
+    info.val = NULL;
+    info.env_show = test_env_show;
+
+    kmpp_env_add(kmpp_env_osal, NULL, &info);
+
+    kmpp_logi("osal env test done\n");
+
+    kmpp_logi("osal test func done\n");
+
+    return rk_ok;
+}
+
+void osal_test_end(void)
+{
+    osal_fs_dev_mgr_deinit(test_fs_dev_mgr);
+    osal_class_deinit(test_cls);
+
+    kmpp_logi("osal class test done\n");
+}
+
+int osal_test_init(void)
+{
+    kmpp_logi("osal test start\n");
+
+    osal_worker_init(&test_worker, "osal test");
+    osal_work_init(&test_work, osal_test_func, NULL);
+
+    osal_work_queue(test_worker, test_work);
+
+    return 0;
+}
+
+void osal_test_exit(void)
+{
+    osal_worker_deinit(&test_worker);
+    osal_work_deinit(&test_work);
+
+    osal_test_end();
+
+    kmpp_logi("osal test end\n");
+}
+
+module_init(osal_test_init);
+module_exit(osal_test_exit);
+
+MODULE_AUTHOR("rockchip");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.0");
