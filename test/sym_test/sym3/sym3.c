ae9c7268423e0b5ec166fcbf3f0b5c4f9b6aba1a
Herman Chen <herman.chen@rock-chips.com>
2024-12-11 17:34:02 +0800

    feat[test]: Add test for kmpp_sym
    Test usage:
    
    1. Prepare symbol test.
       insmod osal.ko and insmod sys.ko
    
    2. Run symbol runtime test.
       insmod sym_test.ko
       The test thread will be always running to detect and use symbols.
       stop test:  echo 1 > /proc/kmpp_debug/sym_test_stop
       enable log: echo 1 > /proc/kmpp_debug/sym_test_log
    
    3. Test runtime symbol status.
       insmod sym1.ko; rmmod sym1
       insmod sym2.ko; rmmod sym2
       insmod sym3.ko; rmmod sym3
       insmod sym4.ko; rmmod sym4
       Loop the insmod rmmod in script.
    
    4. Stop symbol runtime test.
       echo 1 > /proc/kmpp_debug/sym_test_stop
       rmmod sym_test
    
    5. rmmod sys and rmmod osal.ko, check memory leak.
    
    Sample test script:
    
    adb shell insmod /sdcard/sym_test.ko
    
    for i in {1..20}
    do
        adb shell insmod /sdcard/sym1.ko
        adb shell insmod /sdcard/sym2.ko
        adb shell insmod /sdcard/sym3.ko
        adb shell insmod /sdcard/sym4.ko
        adb shell rmmod sym2
        adb shell rmmod sym1
        adb shell rmmod sym3
        adb shell rmmod sym4
    done
    for i in {1..20}
    do
        adb shell insmod /sdcard/sym1.ko
        adb shell rmmod sym1
        adb shell insmod /sdcard/sym2.ko
        adb shell rmmod sym2
        adb shell insmod /sdcard/sym3.ko
        adb shell rmmod sym3
        adb shell insmod /sdcard/sym4.ko
        adb shell rmmod sym4
    done
    
    adb shell "echo 1 > /proc/kmpp_debug/sym_test_stop"
    adb shell rmmod sym_test
    
    Change-Id: Ia723373762f65f126f9262f979da3bea50cd9b75
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

---
+++ test/sym_test/sym3/sym3.c
@@ -0,0 +1,214 @@
+/* SPDX-License-Identifier: ((GPL-2.0+ WITH Linux-syscall-note) OR MIT) */
+/*
+ * Copyright (c) 2024 Rockchip Electronics Co., Ltd.
+ */
+
+#define MODULE_TAG "sym3"
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include "kmpp_osal.h"
+#include "kmpp_sym.h"
+
+typedef struct SymInfo_t {
+    const rk_u8 *name;
+    KmppFuncPtr func;
+} SymInfo;
+
+static osal_worker *test_worker = NULL;
+static osal_work *test_work = NULL;
+static KmppSymDef def = NULL;
+static KmppSyms sym_test = NULL;
+static KmppSym sym_debug = NULL;
+
+static rk_s32 symbol_s32 = 0x12345678;
+static rk_u32 symbol_u32 = 0x23456789;
+static rk_s64 symbol_s64 = 0x3456789abcdef;
+static rk_u64 symbol_u64 = 0x456789abcdef12;
+static void *symbol_ptr = (void *)&test_worker;
+
+#define CONAT(prefix, sufix) prefix##_##sufix
+
+#define SYMBOL_ACCESS(sym, pr_sym) \
+static rk_s32 CONAT(sym, set)(void *in, void *out, const rk_u8 *caller) \
+{ \
+    rk_s32 ret = rk_nok; \
+    if (in) { \
+        typeof(sym) val = sym; \
+        rk_u32 debug = 0; \
+        kmpp_sym_run(sym_debug, &debug, NULL); \
+        kmpp_logi_c(debug, MODULE_TAG #sym " set " #pr_sym " -> " #pr_sym "\n", sym, val); \
+        sym = val; \
+        ret = rk_ok; \
+    } \
+    if (out) \
+        *(rk_s32 *)out = ret; \
+    return rk_ok; \
+} \
+static rk_s32 CONAT(sym, get)(void *in, void *out, const rk_u8 *caller) \
+{ \
+    rk_s32 ret = rk_nok; \
+    if (in) { \
+        typeof(sym) *val = (typeof(sym) *)in; \
+        rk_u32 debug = 0; \
+        kmpp_sym_run(sym_debug, &debug, NULL); \
+        kmpp_logi_c(debug, MODULE_TAG #sym " get " #pr_sym " -> " #pr_sym "\n", *val, sym); \
+        *val = sym; \
+        ret = rk_ok; \
+    } \
+    if (out) \
+        *(rk_s32 *)out = ret; \
+    return rk_ok; \
+}
+
+SYMBOL_ACCESS(symbol_s32, %x)
+SYMBOL_ACCESS(symbol_u32, %x)
+SYMBOL_ACCESS(symbol_s64, %llx)
+SYMBOL_ACCESS(symbol_u64, %llx)
+SYMBOL_ACCESS(symbol_ptr, %px)
+
+static rk_s32 symbol_funcion(void *in, void *out, const rk_u8 *caller)
+{
+    rk_u32 debug = 0;
+    kmpp_sym_run(sym_debug, &debug, NULL);
+
+    kmpp_logi_c(debug, MODULE_TAG " test func enter\n");
+    kmpp_logi_c(debug, MODULE_TAG " s32 %x\n", symbol_s32);
+    kmpp_logi_c(debug, MODULE_TAG " u32 %x\n", symbol_u32);
+    kmpp_logi_c(debug, MODULE_TAG " s64 %llx\n", symbol_s64);
+    kmpp_logi_c(debug, MODULE_TAG " u64 %llu\n", symbol_u64);
+    kmpp_logi_c(debug, MODULE_TAG " ptr %px\n", symbol_ptr);
+
+    return rk_ok;
+}
+
+
+static SymInfo sym_info[] = {
+    {   "set_s32",  symbol_s32_set, },
+    {   "get_s32",  symbol_s32_get, },
+    {   "set_u32",  symbol_u32_set, },
+    {   "get_u32",  symbol_u32_get, },
+    {   "set_s64",  symbol_s64_set, },
+    {   "get_s64",  symbol_s64_get, },
+    {   "set_u64",  symbol_u64_set, },
+    {   "get_u64",  symbol_u64_get, },
+    {   "set_ptr",  symbol_ptr_set, },
+    {   "get_ptr",  symbol_ptr_get, },
+    {   "run_func", symbol_funcion  },
+};
+
+static rk_s32 sym_test_func(const char *syms_name)
+{
+    KmppSyms syms = NULL;
+    KmppSym sym = NULL;
+    rk_s32 ret = rk_nok;
+    rk_s32 i;
+
+    ret = kmpp_syms_get(&syms, syms_name);
+
+    kmpp_logi(MODULE_TAG " try get syms %s ret %px\n", syms_name, syms);
+
+    if (!syms)
+        return ret;
+
+    for (i = 0; i < ARRAY_SIZE(sym_info); i++) {
+        SymInfo *info = &sym_info[i];
+
+        kmpp_sym_get(&sym, syms, info->name);
+        if (!sym) {
+            kmpp_logi(MODULE_TAG " get sym %s:%s failed\n", syms_name, info->name);
+            continue;
+        }
+
+        ret = kmpp_sym_run_f(sym, NULL, NULL, MODULE_TAG);
+
+        kmpp_logi(MODULE_TAG " run sym %s:%s ret %d\n", syms_name, info->name, ret);
+
+        kmpp_sym_put(sym);
+    }
+
+    kmpp_syms_put(syms);
+
+    return ret;
+}
+
+rk_s32 symbol_func(void *param)
+{
+    rk_s32 i;
+
+    kmpp_logi(MODULE_TAG " test func start\n");
+
+    kmpp_symdef_get(&def, MODULE_TAG);
+
+    kmpp_logi(MODULE_TAG " test func add sym\n");
+
+    for (i = 0; i < ARRAY_SIZE(sym_info); i++) {
+        SymInfo *info = &sym_info[i];
+
+        kmpp_symdef_add(def, info->name, info->func);
+    }
+
+    kmpp_symdef_install(def);
+
+    if (0) {
+        sym_test_func("sym1");
+        sym_test_func("sym2");
+        sym_test_func("sym3");
+        sym_test_func("sym4");
+    }
+
+    kmpp_logi(MODULE_TAG " test func done\n");
+
+    return rk_ok;
+}
+
+void symbol_end(void)
+{
+    if (def) {
+        kmpp_symdef_uninstall(def);
+        kmpp_symdef_put(def);
+        def = NULL;
+    }
+    if (sym_debug) {
+        kmpp_sym_put(sym_debug);
+        sym_debug = NULL;
+    }
+    if (sym_test) {
+        kmpp_syms_put(sym_test);
+        sym_test = NULL;
+    }
+}
+
+int symbol_init(void)
+{
+    kmpp_logi(MODULE_TAG " test start\n");
+
+    kmpp_syms_get(&sym_test, "sym_test");
+    if (sym_test)
+        kmpp_sym_get(&sym_debug, sym_test, "sys_test_get_debug");
+
+    osal_worker_init(&test_worker, MODULE_TAG " test");
+    osal_work_init(&test_work, symbol_func, NULL);
+
+    osal_work_queue(test_worker, test_work);
+
+    return 0;
+}
+
+void symbol_exit(void)
+{
+    osal_worker_deinit(&test_worker);
+    osal_work_deinit(&test_work);
+
+    symbol_end();
+
+    kmpp_logi(MODULE_TAG " test end\n");
+}
+
+module_init(symbol_init);
+module_exit(symbol_exit);
+
+MODULE_AUTHOR("rockchip");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.0");
---
+++ test/sym_test/sym_test/sym_test.c
@@ -0,0 +1,190 @@
+/* SPDX-License-Identifier: ((GPL-2.0+ WITH Linux-syscall-note) OR MIT) */
+/*
+ * Copyright (c) 2024 Rockchip Electronics Co., Ltd.
+ */
+
+#define MODULE_TAG "sym_test"
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include "kmpp_osal.h"
+#include "kmpp_sym.h"
+
+static osal_worker *test_worker = NULL;
+static osal_work *test_work = NULL;
+static KmppEnvNode sym_test_stop_env = NULL;
+static KmppEnvNode sym_test_log_env = NULL;
+static KmppSym sym_test = NULL;
+
+static const rk_u8 *syms_names[] = {
+    "sym1",
+    "sym2",
+    "sym3",
+    "sym4",
+};
+static const rk_s32 syms_count = sizeof(syms_names) / sizeof(syms_names[0]);
+
+static const rk_u8 *sym_names[] = {
+    "set_s32",
+    "get_s32",
+    "set_u32",
+    "get_u32",
+    "set_s64",
+    "get_s64",
+    "set_u64",
+    "get_u64",
+    "set_ptr",
+    "get_ptr",
+    "run_func",
+};
+
+static const rk_s32 sym_count = sizeof(sym_names) / sizeof(sym_names[0]);
+static volatile rk_u32 sym_test_stop = 0;
+static volatile rk_u32 sym_test_log = 0;
+
+static rk_s32 sym_test_func(const rk_u8 *syms_name, rk_s32 *status)
+{
+    KmppSyms syms = NULL;
+    KmppSym sym = NULL;
+    rk_s32 ret = rk_nok;
+    rk_s32 i;
+
+    ret = kmpp_syms_get(&syms, syms_name);
+    if (ret || !syms) {
+        kmpp_logi(MODULE_TAG " try get syms %s failed ret %px\n", syms_name, ret);
+        return ret;
+    }
+
+    for (i = 0; i < sym_count; i++) {
+        const rk_u8 *name = sym_names[i];
+        rk_s32 valid = 0;
+
+        ret = kmpp_sym_get(&sym, syms, name);
+        if (!ret && sym) {
+            ret = kmpp_sym_run_f(sym, NULL, NULL, MODULE_TAG);
+            if (!ret)
+                valid = 1;
+        }
+
+        if (status[i] != valid) {
+            kmpp_logi(MODULE_TAG " sym %s:%s changed %s -> %s\n", syms_name, name,
+                      status[i] ? "valid" : "invalid", valid ? "valid" : "invalid");
+            status[i] = valid;
+        }
+
+        kmpp_sym_put(sym);
+    }
+
+    kmpp_syms_put(syms);
+
+    return ret;
+}
+
+rk_s32 symbol_func(void *param)
+{
+    rk_s32 *status;
+    rk_s32 i;
+
+    kmpp_logi(MODULE_TAG " test func start\n");
+
+    status = kmpp_calloc(sizeof(rk_s32) * syms_count * sym_count);
+    if (!status) {
+        kmpp_loge(MODULE_TAG " kmpp_calloc status failed\n");
+        return rk_nok;
+    }
+
+    while (!sym_test_stop) {
+        for (i = 0; i < syms_count; i++) {
+            sym_test_func(syms_names[i], &status[i * sym_count]);
+            osal_msleep(50);
+        }
+    }
+
+    kmpp_logi(MODULE_TAG " test func finish\n");
+
+    kmpp_free(status);
+
+    return rk_ok;
+}
+
+static rk_s32 sys_test_get_debug(void *in, void *out, const rk_u8 *caller)
+{
+    rk_u32 *debug = (rk_u32 *)in;
+
+    if (debug)
+        *debug = sym_test_log;
+
+    return rk_ok;
+}
+
+void symbol_end(void)
+{
+    if (sym_test_stop_env) {
+        kmpp_env_del(kmpp_env_debug, sym_test_stop_env);
+        sym_test_stop_env = NULL;
+    }
+    if (sym_test_log_env) {
+        kmpp_env_del(kmpp_env_debug, sym_test_log_env);
+        sym_test_log_env = NULL;
+    }
+    if (sym_test) {
+        kmpp_symdef_uninstall(sym_test);
+        kmpp_symdef_put(sym_test);
+        sym_test = NULL;
+    }
+}
+
+int symbol_init(void)
+{
+    KmppEnvInfo env;
+
+    kmpp_logi(MODULE_TAG " test start\n");
+
+    env.type = KmppEnv_u32;
+    env.readonly = 0;
+    env.name = "sym_test_stop";
+    env.val = (void *)&sym_test_stop;
+    env.env_show = NULL;
+
+    kmpp_env_add(kmpp_env_debug, &sym_test_stop_env, &env);
+
+    env.type = KmppEnv_u32;
+    env.readonly = 0;
+    env.name = "sym_test_log";
+    env.val = (void *)&sym_test_log;
+    env.env_show = NULL;
+
+    kmpp_env_add(kmpp_env_debug, &sym_test_log_env, &env);
+
+    kmpp_symdef_get(&sym_test, MODULE_TAG);
+    /* Add sys export funciton here */
+    if (sym_test) {
+        kmpp_symdef_add(sym_test, "sys_test_get_debug", sys_test_get_debug);
+        kmpp_symdef_install(sym_test);
+    }
+
+    osal_worker_init(&test_worker, MODULE_TAG " test");
+    osal_work_init(&test_work, symbol_func, NULL);
+
+    osal_work_queue(test_worker, test_work);
+
+    return 0;
+}
+
+void symbol_exit(void)
+{
+    osal_worker_deinit(&test_worker);
+    osal_work_deinit(&test_work);
+
+    symbol_end();
+
+    kmpp_logi(MODULE_TAG " test end\n");
+}
+
+module_init(symbol_init);
+module_exit(symbol_exit);
+
+MODULE_AUTHOR("rockchip");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.0");
