8b97b6e05028a9e241c8445592afd25eec44f6e5
Hongjin Li <vic.hong@rock-chips.com>
2024-12-31 11:49:02 +0800

    fix[kmpp_atomic]: Using zero-length arrays
    Signed-off-by: Hongjin Li <vic.hong@rock-chips.com>
    Change-Id: Ia434f4f33cd67ea6f67e7ddbccbd8a1cdc86c30e

--- osal/kmpp_atomic.c
+++ osal/kmpp_atomic.c
@@ -12,7 +12,7 @@
 
 struct osal_atomic_t {
     rk_u32 need_free;
-    atomic_t *atomic;
+    atomic_t atomic[];
 };
 
 rk_s32 osal_atomic_size(void)
@@ -32,7 +32,6 @@ rk_s32 osal_atomic_init(osal_atomic **atomic)
             return rk_nok;
 
         p->need_free = 1;
-        p->atomic = (atomic_t *)(p + 1);
 
         *atomic = p;
         return 0;
@@ -47,7 +46,6 @@ rk_s32 osal_atomic_assign(osal_atomic **atomic, void *buf, rk_s32 size)
     if (atomic && buf && size >= osal_atomic_size()) {
         osal_atomic *p = (osal_atomic *)buf;
 
-        p->atomic = (atomic_t *)(p + 1);
         p->need_free = 0;
 
         *atomic = p;
@@ -75,7 +73,7 @@ EXPORT_SYMBOL(osal_atomic_deinit);
 
 rk_s32 osal_atomic_read(osal_atomic *v)
 {
-    if (v && v->atomic)
+    if (v)
         return atomic_read(v->atomic);
 
     return -EINVAL;
@@ -84,14 +82,14 @@ EXPORT_SYMBOL(osal_atomic_read);
 
 void osal_atomic_set(osal_atomic *v, rk_s32 val)
 {
-    if (v && v->atomic)
+    if (v)
         atomic_set(v->atomic, val);
 }
 EXPORT_SYMBOL(osal_atomic_set);
 
 rk_s32 osal_atomic_fetch_inc(osal_atomic *v)
 {
-    if (v && v->atomic)
+    if (v)
         return atomic_fetch_inc(v->atomic);
 
     return -EINVAL;
@@ -100,7 +98,7 @@ EXPORT_SYMBOL(osal_atomic_fetch_inc);
 
 rk_s32 osal_atomic_fetch_dec(osal_atomic *v)
 {
-    if (v && v->atomic)
+    if (v)
         return atomic_fetch_dec(v->atomic);
 
     return -EINVAL;
@@ -109,7 +107,7 @@ EXPORT_SYMBOL(osal_atomic_fetch_dec);
 
 rk_s32 osal_atomic_inc_return(osal_atomic *v)
 {
-    if (v && v->atomic)
+    if (v)
         return atomic_inc_return(v->atomic);
 
     return -EINVAL;
@@ -118,7 +116,7 @@ EXPORT_SYMBOL(osal_atomic_inc_return);
 
 rk_s32 osal_atomic_dec_return(osal_atomic *v)
 {
-    if (v && v->atomic)
+    if (v)
         return atomic_dec_return(v->atomic);
 
     return -EINVAL;
@@ -127,7 +125,7 @@ EXPORT_SYMBOL(osal_atomic_dec_return);
 
 rk_s32 osal_atomic_dec_and_test(osal_atomic *v)
 {
-    if (v && v->atomic)
+    if (v)
         return atomic_dec_and_test(v->atomic);
 
     return -EINVAL;
@@ -136,7 +134,7 @@ EXPORT_SYMBOL(osal_atomic_dec_and_test);
 
 rk_s32 osal_atomic_xchg(osal_atomic *v, rk_s32 i)
 {
-    if (v && v->atomic)
+    if (v)
         return atomic_xchg(v->atomic, i);
 
     return -EINVAL;
