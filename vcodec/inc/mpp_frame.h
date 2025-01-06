92c1d9172f195dcb2075fb36d1a65ba4e970ca7c
Yandong Lin <yandong.lin@rock-chips.com>
2025-01-06 16:01:08 +0800

    feat[mpp_frame]: Add tile format flag
    From commit:
    206614: feat[mpp_frame]: Add tile format flag | https://10.10.10.29/c/rk/mpp/+/206614
    
    Change-Id: Ie5881be9cbebcb9197ef820499822fcd3823726d
    Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>

--- vcodec/inc/mpp_frame.h
+++ vcodec/inc/mpp_frame.h
@@ -164,6 +164,8 @@ typedef enum {
 
 #define MPP_FRAME_FBC_MASK          (0x00f00000)
 #define MPP_FRAME_FBC_NONE          (0x00000000)
+
+#define MPP_FRAME_TILE_FLAG         (0x02000000)
 /*
  * AFBC_V1 is for ISP output.
  * It has default payload offset to be calculated * from width and height:
@@ -191,6 +193,8 @@ typedef enum {
 
 #define MPP_FRAME_FMT_IS_LE(fmt)    ((fmt & MPP_FRAME_FMT_LE_MASK) == MPP_FRAME_FMT_LE_MASK)
 #define MPP_FRAME_FMT_IS_BE(fmt)    ((fmt & MPP_FRAME_FMT_LE_MASK) == 0)
+
+#define MPP_FRAME_FMT_IS_TILE(fmt)  (fmt & MPP_FRAME_TILE_FLAG)
 
 /* mpp color format index definition */
 typedef enum {
