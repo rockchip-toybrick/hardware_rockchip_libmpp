14f961602913c9a96998276ebad8df9d46d38966
Herman Chen <herman.chen@rock-chips.com>
2025-02-11 16:37:01 +0800

    feat[vsp]: Add chan id api
    Change-Id: I0cb82a89d5a7cdb542f983611b8580c9ca4d60ba
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- include/kmpi_vsp.h
+++ include/kmpi_vsp.h
@@ -20,26 +20,56 @@ typedef void* KmppVspStCfg;
 /* runtime config */
 typedef void* KmppVspRtCfg;
 
-/* channel status change sync operation and allow bulk operation */
+/*
+ * There are two sets for Video Signal Processer (VSP) interfaces:
+ * context interface and chan id interface
+ *
+ * Each set of interface has the same interface functions:
+ *
+ * init / deinit
+ * - video signal processer instance create / destroy interfaces.
+ *
+ * reset / start / stop / ctrl
+ * - video signal processer control flow interfaces.
+ *
+ * cfg / rt_cfg
+ * - video signal processer static parameter and runtime parameter interfaces.
+ *
+ * encode / put_frm / get_pkt
+ * - video signal processer data flow interfaces.
+ */
+
 rk_s32 kmpp_vsp_init(KmppCtx *ctx, KmppVspInitCfg cfg);
 rk_s32 kmpp_vsp_deinit(KmppCtx ctx, KmppVspDeinitCfg cfg);
+rk_s32 kmpp_vsp_chan_init(KmppChanId *id, KmppVspInitCfg cfg);
+rk_s32 kmpp_vsp_chan_deinit(KmppChanId id, KmppVspDeinitCfg cfg);
 
 rk_s32 kmpp_vsp_reset(KmppCtx ctx, KmppVspResetCfg cfg);
 rk_s32 kmpp_vsp_start(KmppCtx ctx, KmppVspStartCfg cfg);
 rk_s32 kmpp_vsp_stop(KmppCtx ctx, KmppVspStopCfg cfg);
-/* runtime query / status check */
 rk_s32 kmpp_vsp_ctrl(KmppCtx ctx, rk_s32 cmd, void *arg);
+rk_s32 kmpp_vsp_chan_reset(KmppChanId id, KmppVspResetCfg cfg);
+rk_s32 kmpp_vsp_chan_start(KmppChanId id, KmppVspStartCfg cfg);
+rk_s32 kmpp_vsp_chan_stop(KmppChanId id, KmppVspStopCfg cfg);
+rk_s32 kmpp_vsp_chan_ctrl(KmppChanId id, rk_s32 cmd, void *arg);
 
 /* static config */
 rk_s32 kmpp_vsp_get_cfg(KmppCtx ctx, KmppVspStCfg cfg);
 rk_s32 kmpp_vsp_set_cfg(KmppCtx ctx, KmppVspStCfg cfg);
+rk_s32 kmpp_vsp_chan_get_cfg(KmppChanId id, KmppVspStCfg cfg);
+rk_s32 kmpp_vsp_chan_set_cfg(KmppChanId id, KmppVspStCfg cfg);
 
 /* runtime config */
 rk_s32 kmpp_vsp_get_rt_cfg(KmppCtx ctx, KmppVspRtCfg cfg);
 rk_s32 kmpp_vsp_set_rt_cfg(KmppCtx ctx, KmppVspRtCfg cfg);
+rk_s32 kmpp_vsp_chan_get_rt_cfg(KmppChanId id, KmppVspStCfg cfg);
+rk_s32 kmpp_vsp_chan_set_rt_cfg(KmppChanId id, KmppVspStCfg cfg);
 
 rk_s32 kmpp_vsp_proc(KmppCtx ctx, KmppFrame in, KmppFrame *out);
 rk_s32 kmpp_vsp_put_frm(KmppCtx ctx, KmppFrame frm);
 rk_s32 kmpp_vsp_get_frm(KmppCtx ctx, KmppFrame *frm);
+rk_s32 kmpp_vsp_chan_proc(KmppChanId id, KmppFrame in, KmppFrame *out);
+rk_s32 kmpp_vsp_chan_put_frm(KmppChanId id, KmppFrame frm);
+rk_s32 kmpp_vsp_chan_get_frm(KmppChanId id, KmppFrame *frm);
 
 #endif /*__KMPI_VSP_H__*/
